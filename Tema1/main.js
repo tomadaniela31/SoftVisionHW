46da5b2a409766e4bd9c17762f6681a03be7ac06
tomadaniela31 <58103023+tomadaniela31@users.noreply.github.com>
2020-11-16 22:40:35 +0200

    Add files via upload

---
+++ Tema1/main.js
@@ -0,0 +1,13 @@
+var book={
+    title:"IT",
+    author:"Stephen King",
+    noPages:456,
+    existInEnglish: true
+}
+var name="hsbjs";
+console.log( book);
+
+function deterNume(name)
+{
+    console.log("Buna, numele meu este "+name);
+}
